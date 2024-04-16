174382940699fd9ebdd49ff15d71525b4a545ae0
Niiranen Milo <niiranenmilo@gmail.com>
2024-04-16 12:13:36 +0300

    Pikku juttu vaan ei tärkeä

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,17 @@
-// Tallenna nykyisen sivun URL-osoite sessionStorageen, kun käyttäjä siirtyy ohjesivulle
+// Tallentaa nykyisen sivun URL-osoitten sessionStorageen, kun käyttäjä siirtyy ohjesivulle
 document.getElementById('help-button').addEventListener('click', function() {
     sessionStorage.setItem('previousPage', window.location.href);
     window.location.href = 'instructions.html'; 
 });
 
-// Ohjaa käyttäjä takaisin edelliselle sivulle tallennetun URL-osoitteen perusteella
+// Ohjaa käyttäjän takaisin edelliselle sivulle tallennetun URL-osoitteen perusteella
 document.getElementById('continue').addEventListener('click', function() {
     const previousPage = sessionStorage.getItem('previousPage');
     if (previousPage) {
         window.location.href = previousPage;
-        sessionStorage.removeItem('previousPage'); // Poista tallennettu URL-osoite
+        sessionStorage.removeItem('previousPage'); // Poista tallennettun URL-osoite
     } else {
-        // Jos edellistä sivua ei ole tallennettu, voit ohjata käyttäjän jonnekin oletussivulle
-        window.location.href = 'karjalanpiirakka.html'; // Korvaa oikealla oletussivun URL-osoitteella
+        // Jos edellistä sivua ei ole tallennettu, ohjaa käyttäjän jonnekin oletussivulle
+        window.location.href = 'karjalanpiirakka.html';
     }
 });
